fe874e7992bdee4362da962881fa295361d2ccd8
TheJP <jp_55555@hotmail.com>
2024-01-27 02:35:45 +0100

    Worked on sofa game

--- Catfluencer/01_sofa_game/sofa_game.gd
+++ Catfluencer/01_sofa_game/sofa_game.gd
@@ -4,7 +4,9 @@ extends Node2D
 func _ready():
 	var new_size: Vector2 = Vector2(1.0, 0.1) * ReferenceSize.rect.size
 	$Slider.fit_into(ReferenceSize.rect.position + ReferenceSize.rect.size - new_size, new_size)
+	$Couch.global_position = ReferenceSize.rect.position + 0.5 * ReferenceSize.rect.size
 
 
 func _on_slider_value_changed(new_value):
-	pass
+	$Couch.global_position = ReferenceSize.rect.position + 0.5 * ReferenceSize.rect.size
+	$Couch.global_position.x += new_value * 0.5 * ReferenceSize.rect.size.x
