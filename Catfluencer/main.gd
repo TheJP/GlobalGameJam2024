351caf95fb41ea1a51eb0838284e35075c8d58cf
TheJP <jp_55555@hotmail.com>
2024-01-27 04:46:03 +0100

    Added credits scene

---
+++ Catfluencer/main.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func _on_start_button_pressed():
+	Level.start()
--- Catfluencer/singleton/level.gd
+++ Catfluencer/singleton/level.gd
@@ -1,13 +1,27 @@
 extends Node
 
 
+var _regular_flow = false
 var _current = 0
 const _levels = [
-	preload("res://01_sofa_game/sofa_game.tscn")
+	preload("res://01_sofa_game/sofa_game.tscn"),
+	preload("res://credits.tscn"),
 ]
 
-func finished_level(success: bool):
-	await get_tree().create_timer(1.0, false).timeout
+
+func start():
+	_regular_flow = true
+	_current = 0
+	get_tree().change_scene_to_packed(_levels[_current])
+
+
+func finished_level(success: bool, should_sleep = true):
+	if should_sleep:
+		await get_tree().create_timer(1.0, false).timeout
+
+	if not _regular_flow:
+		get_tree().reload_current_scene()
+		return
 
 	if success:
 		_current = (_current + 1) % len(_levels)
