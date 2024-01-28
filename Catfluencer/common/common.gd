f1a8682e988179ce6913116ceb2c45b20a86490f
TheJP <jp_55555@hotmail.com>
2024-01-28 15:52:19 +0100

    Added control bar and pause menu

--- Catfluencer/common/common.gd
+++ Catfluencer/common/common.gd
@@ -4,6 +4,7 @@ extends Node2D
 @export var show_viewer_count: bool = true
 @export var show_chat: bool = true
 @export var show_webcam: bool = true
+@export var show_controls: bool = true
 
 
 var emotion_textures = {
@@ -25,9 +26,49 @@ func _ready():
 	%OnlineBackground.visible = show_webcam
 	%Emotion.visible = show_webcam
 	%Mic.visible = show_webcam
+	%ControlsContainer.visible = show_controls
 	Level.show_boring.connect(func(): $Boring.visible = true)
 
 
 func _process(delta):
 	%Viewers.text = "%.0f" % (Level.remaining_time * 100)
 	%Emotion.texture = emotion_textures[Reactions.current]
+
+
+func _on_play_button_pressed():
+	unpause()
+
+
+func _on_pause_button_pressed():
+	pause()
+
+
+func _on_resume_button_pressed():
+	unpause()
+
+
+func _on_close_button_pressed():
+	unpause()
+
+
+func _on_restart_button_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://main/main.tscn")
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+
+func pause():
+	get_tree().paused = true
+	%PlayButton.visible = true
+	%PauseButton.visible = false
+	%PauseMenu.visible = true
+
+
+func unpause():
+	get_tree().paused = false
+	%PlayButton.visible = false
+	%PauseButton.visible = true
+	%PauseMenu.visible = false
